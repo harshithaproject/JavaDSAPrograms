c6a1ee7dc34160f96b93661ee2cdc5e813f0f084
harshithaaditham <harshithaaditham8398@gmail.com>
2026-01-24 14:17:44 +0530

    JavaDSAPrograms

--- src/Arrays/ArraySum.java
+++ src/Arrays/ArraySum.java
@@ -3,6 +3,7 @@ package Arrays;
 import java.util.Arrays;
 
 public class ArraySum {
+
     public static void main(String[] args) {
         int[] arr = {1,2,3,4};
         int[] newArr = new int[arr.length];
