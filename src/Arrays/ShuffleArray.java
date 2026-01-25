745a2ea874c501711dded8d86645c3a7a96d78b1
harshithaaditham <harshithaaditham8398@gmail.com>
2026-01-25 11:01:30 +0530

    JavaDSAPrograms Day2

--- src/Arrays/ShuffleArray.java
+++ src/Arrays/ShuffleArray.java
@@ -3,20 +3,21 @@ import java.util.*;
 
 public class ShuffleArray {
     public static void main(String[] args) {
-        int[] arr = {2,5,1,3,4,7};
+        int[] arr = {2, 5, 1, 3, 4, 7};
         int[] newArr = new int[arr.length];
-        int n=3;
+        int left = 0;
+        int right = left +3;
+
+        for (int i = 0; i < arr.length; i++) {
+                newArr[i] = arr[left];
+                left++;
+                newArr[i] = arr[right];
+                right++;
 
 
-
-        for(int i=0;i<arr.length;i++) {
-            for(int j =i+n;j< arr.length;j++){
-
-                newArr[i]=arr[j];
-            }
         }
         System.out.println(Arrays.toString(newArr));
 
-
     }
+
 }
---
+++ src/Arrays/TwoPointerApproach.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class TwoPointerApproach {
+    public static void main(String[] args) {
+        //For two pointer approach the array will be sorted and then it will work efficiently
+        //If not sorted then sort the array and then use two pointer approach.
+        int[] arr = {};
+
+
+    }
+}
