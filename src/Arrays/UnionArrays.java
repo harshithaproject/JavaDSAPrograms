73c446e7ab542f8782d374f199bea6b2a01eaa86
Harshitha Aditham <47668303+harshithaproject@users.noreply.github.com>
2026-01-26 16:54:30 +0530

    JavaDSAPrograms Day3

---
+++ src/Arrays/UnionArrays.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.*;
+
+public class UnionArrays {
+    public static void main(String[] args) {
+        int [] num1 ={1,2,3,4,7};
+        int [] num2 ={1,2,5};
+//        System.out.println(num1.length);
+//        System.out.println(num2.length);
+        int length = num1.length+num2.length;
+        int[] num3 = new int[length];
+
+
+
+
+
+
+        System.out.println(Arrays.toString(num3));
+    }
+}
