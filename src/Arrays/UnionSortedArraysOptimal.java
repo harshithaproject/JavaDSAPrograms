9007e2a85d0951245a76aed016ba970b85a06702
Harshitha Aditham <47668303+harshithaproject@users.noreply.github.com>
2026-02-15 11:39:35 +0530

    JavaDSAPrograms Day10

---
+++ src/Arrays/UnionSortedArraysOptimal.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.*;
+
+public class UnionSortedArraysOptimal {
+    public static void main(String[] args) {
+        //Two pointer approach
+        int t1 [] = {1,1,2,3,4,5};
+        int t2 [] = {2,3,4,4,5,6};
+        //Union sorted arrays will be sorted originally in both the arrays will be combined into a single array which
+        //will not include the duplicate elements
+        int i= t1[0];
+        int j= t2[0];
+
+        while(i<j){
+            if(t1[i]==t1[j]){
+
+            }
+        }
+
+    }
+}
